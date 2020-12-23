be4483487a12514998bf1d767e3dea7d31698d47
EmmyM_ighty <remi.emma04@gmail.com>
2020-12-23 23:56:54 +0100

    Revert

---
+++ build/precache-manifest.669d9e84d787363e6f791edfdeb90879.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ba9d0dd945c5896dd348c62442243c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b731388095ad584a0cbd",
+    "url": "/static/css/main.934b7168.chunk.css"
+  },
+  {
+    "revision": "ad2987ec11121b947e67",
+    "url": "/static/js/2.14f92628.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.14f92628.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b731388095ad584a0cbd",
+    "url": "/static/js/main.d0759381.chunk.js"
+  },
+  {
+    "revision": "3d3368e6f1c7ed77ecd3",
+    "url": "/static/js/runtime-main.dc670065.js"
+  },
+  {
+    "revision": "2cf1493e68315d6b097294def1390902",
+    "url": "/static/media/togaj_logo.2cf1493e.png"
+  },
+  {
+    "revision": "8f901228fab809cc8213fb037efa1fa9",
+    "url": "/static/media/togaj_logo_edited.8f901228.png"
+  }
+]);
